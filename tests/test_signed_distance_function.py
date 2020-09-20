b2dc6b6782e2ff5d6747ca531be89dcdbe80a2b4
Keith Roberts <krober10@nd.edu>
2020-09-20 17:53:16 -0300

    fixing test

--- tests/test_signed_distance_function.py
+++ tests/test_signed_distance_function.py
@@ -1,6 +1,6 @@
 import os
 
-from oceanmesh import Shoreline, signed_distance_function
+from oceanmesh import Shoreline, signed_distance_function, Domain
 
 
 def test_signed_distance_function():
@@ -12,3 +12,5 @@ def test_signed_distance_function():
     shp = Shoreline(fname, bbox, h0)
 
     domain = signed_distance_function(shp)
+
+    assert isinstance(domain, Domain)
