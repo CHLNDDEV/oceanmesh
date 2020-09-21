935a6a1970e025b189e9a09a90ba4da4fc2e887a
Keith Roberts <krober10@nd.edu>
2020-09-21 20:54:19 -0300

    tidying up SDF

--- oceanmesh/signed_distance_function.py
+++ oceanmesh/signed_distance_function.py
@@ -38,11 +38,18 @@ def signed_distance_function(shoreline):
     poly = numpy.vstack((shoreline.inner, shoreline.mainland, shoreline.boubox))
     tree = scipy.spatial.cKDTree(poly[~numpy.isnan(poly[:, 0]), :])
     e = edges.get_poly_edges(poly)
+    e_box = edges.get_poly_edges(shoreline.boubox)
 
     def func(x):
-        dist, _ = tree.query(x, k=1)
-        inside_shoreline, _ = inpoly(x, poly, e)
-        dist[inside_shoreline == 1] *= -1.0
+        # Initialize d with some positive number larger than geps
+        dist = numpy.zeros(len(x)) + 1.0
+        in_boubox, _ = inpoly(x, shoreline.boubox, e_box)
+        # for points inside boubox, compute dist to shoreline
+        if numpy.sum(in_boubox) > 0:
+            in_outer, _ = inpoly(x[in_boubox == 1], poly, e)
+            d, _ = tree.query(x[in_boubox == 1], k=1)
+        # d is signed negative if inside and vice versa.
+        dist[in_boubox == 1] = (-1) ** (in_outer) * d
         return dist
 
     return Domain(shoreline.bbox, func)
