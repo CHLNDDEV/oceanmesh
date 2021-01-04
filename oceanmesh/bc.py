c6464517bfab4b55571dd207854283def410c056
Keith Roberts <krober10@nd.edu>
2021-01-03 22:29:58 -0300

    returning in adcirc format

--- oceanmesh/bc.py
+++ oceanmesh/bc.py
@@ -11,13 +11,12 @@ def label_island_bcs(vertices, faces, ibtype=21):
     """
     boundary_loops = edges.get_winded_boundary_loops(faces)
     nbou = len(boundary_loops[1:])
-    nvell = np.zeros((1, nbou))
-    max_len = [max(len(loop) for loop in boundary_loops[1:])]
-    nbvv = np.zeros((max_len, nbou))
+    nvell = np.zeros(nbou, dtype=int)
+    max_len = max(len(loop[:-1].ravel()) for loop in boundary_loops[1:])
+    nbvv = np.zeros((max_len, nbou), dtype=int)
     nvel = 0
     for nbou, loop in enumerate(boundary_loops[1:]):
-        vso = vertices[loop]
-        nvell[nbou] = len(vso)
+        nvell[nbou] = len(loop[:-1].ravel())
         nvel += nvell[nbou]
-        nbvv[: nvell[nbou], nbou] = loop
-    return nvell, nvel, nbvv
+        nbvv[: nvell[nbou], nbou] = loop[:-1].ravel()
+    return nvel, nvell, nbvv
