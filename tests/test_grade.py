41212554b508222fcdb148c03c5a84f78e36c3e9
Keith Roberts <krober@usp.edu>
2021-07-29 18:33:31 -0300

    fix incorrect test

--- tests/test_grade.py
+++ tests/test_grade.py
@@ -1,12 +1,14 @@
- --import oceanmesh as om
+import oceanmesh as om
 
-   fname = "gshhg-shp-2.3.7/GSHHS_shp/f/GSHHS_f_L1.shp"
 
-   bbox, min_edge_length = (-75.000, -70.001, 40.0001, 41.9000), 1e3
+def test_grade():
+    fname = "gshhg-shp-2.3.7/GSHHS_shp/f/GSHHS_f_L1.shp"
 
-   shore = om.Shoreline(fname, bbox, min_edge_length)
+    bbox, min_edge_length = (-75.000, -70.001, 40.0001, 41.9000), 1e3
 
-   edge_length = om.distance_sizing_function(shore, rate=0.35)
+    shore = om.Shoreline(fname, bbox, min_edge_length)
 
-   test_edge_length = om.enforce_mesh_gradation(edge_length, gradation=0.20)
-   test_edge_length.plot(show=False, file_name="test_edge_length.png")
+    edge_length = om.distance_sizing_function(shore, rate=0.35)
+
+    test_edge_length = om.enforce_mesh_gradation(edge_length, gradation=0.20)
+    test_edge_length.plot(show=False, file_name="test_edge_length.png")
