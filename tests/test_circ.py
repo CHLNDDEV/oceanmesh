f7dffb4174300b3a76f265f458d5699407ce4cf9
Keith Roberts <krober@usp.br>
2021-08-29 22:35:06 -0400

    linting tests

--- tests/test_circ.py
+++ tests/test_circ.py
@@ -15,28 +15,36 @@ def test_circ():
         n = len(shapes)
         for shape in shapes:
             if n > 1:
-                print('WARN, {:d} polygons in \'{:s}\'. Continue with item[0].'.format(n, os.path.basename(shp0)))
+                print(
+                    "WARN, {:d} polygons in '{:s}'. Continue with item[0].".format(
+                        n, os.path.basename(shp0)
+                    )
+                )
                 break
 
         bbox = numpy.asarray(shape.points)
         bbox = numpy.append(bbox, [[numpy.nan, numpy.nan]], axis=0)
 
-    del(shapes, shape, shpf)
+    del (shapes, shape, shpf)
 
-    min_edge_length = 2.e3  # h0
-    max_edge_length = 10.e3
+    min_edge_length = 2.0e3  # h0
+    max_edge_length = 10.0e3
 
     shore = oceanmesh.Shoreline(shp, bbox, min_edge_length)
-    edge_length = oceanmesh.distance_sizing_function(shore, rate=0.10, max_edge_length=max_edge_length)
+    edge_length = oceanmesh.distance_sizing_function(
+        shore, rate=0.10, max_edge_length=max_edge_length
+    )
     domain = oceanmesh.signed_distance_function(shore)
 
     points, cells = oceanmesh.generate_mesh(domain, edge_length)
 
     pyplot.figure(1)
     pyplot.clf()
-    pyplot.triplot(points[:, 0], points[:, 1], cells, '-', lw=0.5, color='0.5')
-    pyplot.plot(shore.boubox[:, 0], shore.boubox[:, 1], '-', color='r', markersize=0)
-    pyplot.plot(shore.inner[:, 0], shore.inner[:, 1], '.', color='gray', markersize=2)
-    pyplot.plot(shore.mainland[:, 0], shore.mainland[:, 1], '-', color='green', linewidth=.5)
-    pyplot.gca().axis('equal')
+    pyplot.triplot(points[:, 0], points[:, 1], cells, "-", lw=0.5, color="0.5")
+    pyplot.plot(shore.boubox[:, 0], shore.boubox[:, 1], "-", color="r", markersize=0)
+    pyplot.plot(shore.inner[:, 0], shore.inner[:, 1], ".", color="gray", markersize=2)
+    pyplot.plot(
+        shore.mainland[:, 0], shore.mainland[:, 1], "-", color="green", linewidth=0.5
+    )
+    pyplot.gca().axis("equal")
     pyplot.show()
--- tests/test_edgefx.py
+++ tests/test_edgefx.py
@@ -42,4 +42,5 @@ def test_edgefx_elevation_bounds():
     edge_length = om.enforce_mesh_size_bounds_elevation(edge_length, dem, bounds)
     edge_length.plot()
 
+
 test_edgefx()
