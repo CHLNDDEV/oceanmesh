9558b3c79d28e7e2bd3feafa9e89f59c4c35cae5
Stefan <stefanzie@gmail.com>
2021-09-12 10:07:31 +1000

    Pytest updated
    Changes
    -------
     [x] Non-existing path to full resolution shape file replaced
         with interim resolution file `tests/GSHHS_i_L1.shp`
     [x] Removed dependency on shoreline database for pytest.
     [x] pyplot.pcolormesh shading option changed to `auto` as
         suggested by matplotlib warning (future release error)

--- tests/test_irregular_domain.py
+++ tests/test_irregular_domain.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.tri as tri
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,7 +6,7 @@ import oceanmesh as om
 
 
 def test_irregular_domain():
-    fname = "gshhg-shp-2.3.7/GSHHS_shp/f/GSHHS_f_L1.shp"
+    fname = os.path.join(os.path.dirname(__file__), "GSHHS_i_L1.shp")
 
     # New York Lower Bay and Jamaica Bay
     bbox = np.array(
