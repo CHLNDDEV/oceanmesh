e4f30f678dd9b9a0e7f83fd9571153b0e5972261
Keith Roberts <krober@usp.br>
2021-09-24 12:45:54 -0400

    fix geodata test

--- tests/test_geodata.py
+++ tests/test_geodata.py
@@ -1,10 +1,9 @@
 import os
 
 import pytest
+from oceanmesh import DEM, Region, Shoreline, edges
 
-from oceanmesh import DEM, Shoreline, edges
-
-fname = os.path.join(os.path.dirname(__file__), "GSHHS_l_L1.shp")
+fname = os.path.join(os.path.dirname(__file__), "GSHHS_i_L1.shp")
 dfname = os.path.join(os.path.dirname(__file__), "galv_sub.nc")
 tfname = os.path.join(os.path.dirname(__file__), "galv_sub.tif")
 
@@ -12,15 +11,16 @@ tfname = os.path.join(os.path.dirname(__file__), "galv_sub.tif")
 @pytest.mark.parametrize(
     "boxes_h0",
     [
-        ((166.0, 176.0, -48.0, -40.0), 1000.0),
-        ((-74.0, -70.0, 35.0, 42.0), 50.0),
+        ((166.0, 176.0, -48.0, -40.0), 0.01),
+        ((-74.0, -70.0, 35.0, 42.0), 0.005),
     ],
 )
 def test_shoreline(boxes_h0):
     """Read in a shapefile at different scales h0
     shoreline and test you get the write output"""
     bbox, h0 = boxes_h0
-    shp = Shoreline(fname, bbox, h0)
+    region = Region(bbox, 4326)
+    shp = Shoreline(fname, region.bbox, h0, crs=region.crs)
     assert len(shp.inner) > 0
     assert len(shp.mainland) > 0
     e = edges.get_poly_edges(shp.inner)
@@ -36,7 +36,7 @@ def test_shoreline(boxes_h0):
         ),
         (
             tfname,
-            (-95.24, -95.00, 29.57, 29.79),
+            (-95.24, -95.21, 28.95, 29.00),
         ),
     ],
 )
@@ -44,5 +44,6 @@ def test_geodata(files_bboxes):
     """Read in a subset of a DEM from netcdf/tif"""
 
     f, bbox = files_bboxes
-    dem = DEM(f, bbox)
+    region = Region(bbox, 4326)
+    dem = DEM(f, bbox=region.bbox, crs=region.crs)
     assert isinstance(dem, DEM), "DEM class did not form"
