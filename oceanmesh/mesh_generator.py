f9d395ba1fba587cb02393ded0ae98579c74e121
Lint Action <lint-action@samuelmeuli.com>
2023-05-27 14:13:16 +0000

    Fix code style issues with Black

--- oceanmesh/mesh_generator.py
+++ oceanmesh/mesh_generator.py
@@ -14,8 +14,7 @@ from .clean import _external_topology
 from .edgefx import multiscale_sizing_function
 from .fix_mesh import fix_mesh
 from .grid import Grid
-from .signed_distance_function import (Domain,
-                                       multiscale_signed_distance_function)
+from .signed_distance_function import Domain, multiscale_signed_distance_function
 
 logger = logging.getLogger(__name__)
 
